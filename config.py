d0c723b63b12cc6b91e93e3dc25428d22bdba7e3
Edgar Martinez <monolet@gmail.com>
2019-03-25 10:31:06 -0600

    AWS production quasi-ready

---
+++ config.py
@@ -0,0 +1,3 @@
+#Postgres Database URI
+#posgresql+psycopg2://user:password@AWSdbEndPoint:port/db_name
+DATABASE_URI='postgresql+psycopg2://dora_edgar:isdoraapp@psqldemostore.cl248trdommz.us-west-2.rds.amazonaws.com:5432/postgresdb'
