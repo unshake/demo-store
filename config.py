936d19c5ca2975195723a8848be26447faa79b58
Edgar Martinez <monolet@gmail.com>
2019-03-25 10:21:36 -0600

    AWS dev ready

---
+++ config.py
@@ -0,0 +1,2 @@
+#SQLite Database URI
+DATABASE_URI='sqlite:///demostore_flask.db?check_same_thread=False'
---
+++ database_setup.py
@@ -0,0 +1,78 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from config import *
+ 
+engine = create_engine(DATABASE_URI)
+Base = declarative_base()
+Base.metadata.create_all(engine)
+ 
+class Users(Base):
+    __tablename__ = 'users'
+   
+    id = Column(String(250), primary_key=True)
+    name = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    keyword = Column(String(250), nullable=False)
+    carrito_total = Column(Integer, nullable=True)
+
+    @property
+    def serialize(self):
+       
+       return {
+           'name'         : self.name,
+           'id'         : self.id,
+       }
+ 
+class Products(Base):
+    __tablename__ = 'products'
+
+
+    name =Column(String(80), nullable = False)
+    id = Column(String(250), primary_key = True)
+    description = Column(String(250), nullable = True)
+    price = Column(Integer, nullable = True)
+    size = Column(String(250), nullable = True)
+    brand = Column(String(250), nullable = True)
+    color = Column(String(250), nullable = True)
+
+    #We added this serialize function to be able to send JSON objects in a serializable format
+    @property
+    def serialize(self):
+       
+       return {
+           'name'         : self.name,
+           'description'         : self.description,
+           'id'         : self.id,
+           'price'         : self.price,
+           'size'         : self.size,
+           'brand'         : self.brand,
+           'color'         : self.color,
+       }
+
+
+class ShoppingCart(Base):
+    __tablename__ = 'Shopping_Cart'
+
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer)
+    total = Column(Integer)
+    user_id = Column(String(250), ForeignKey('users.id'))
+    user = relationship(Users)
+
+class tokenBlackList(Base):
+    __tablename__ = 'tokenBlackList'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    token = Column(String(250), nullable=False)
+    
+
+
+ 
+
+
+ 
+
+Base.metadata.create_all(engine)
